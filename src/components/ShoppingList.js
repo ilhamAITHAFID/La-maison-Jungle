985ff34d28066156f2f454185ca5f81ebe7de184
ILHAM AITHAFID <i.ait_hafid@mundiapolis.ma>
2022-10-17 15:38:46 +0100

    commit 2

---
+++ src/components/ShoppingList.js
@@ -0,0 +1,34 @@
+import {plantList} from '../datas/plantList'
+import '../styles/ShoppingList.css'
+import PlantItem from './PlantItem'
+
+function ShoppingList(){
+    const categories = plantList.reduce(
+        (acc, plant)=> 
+        acc.includes(plant.category)? acc:acc.concat(plant.category),[]
+    )
+    return (
+        <div>
+            <ul>
+                {categories.map((cat)=>(
+                    <li key={cat}>{cat}</li>
+
+                ))}
+            </ul>
+            <ul className='lmj-plant-list'>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						id={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
+				))}
+			</ul>
+            
+        </div>
+    )
+
+}
+ export default ShoppingList
