aca422cb5ecd70b0ebadd6a9ee7fe6c38b71c172
ILHAM AITHAFID <i.ait_hafid@mundiapolis.ma>
2022-10-23 06:58:44 +0100

    control form

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,3 +1,5 @@
+/* Formulaire non controlée
+
 function QuestionForm()
 {
     function handleSubmit(e) {
@@ -9,6 +11,22 @@ function QuestionForm()
         <input type='text' name='my_input' defaultValue='Tapez votre texte' />
         <button type='submit'>Entrer</button>
     </form>)
+}*/
+
+import { useState } from "react"
+
+/**Formulaire controlée */
+function QuestionForm(){
+    const[inputValue, setInputValue]= useState('Posez votre question ici')
+    return(
+        <div>
+            <textarea
+               value={inputValue}
+               onChange={ (e)=>setInputValue(e.target.value)}
+            />
+            <button onClick={()=>alert(inputValue)}>Alertéz moi </button>
+        </div>
+    )
 }
 
 export default QuestionForm
